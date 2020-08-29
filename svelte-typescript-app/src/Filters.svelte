2afac0bc6c8f341852b323d4705abdff3a6d7be4
mejgun <mejgun@gmail.com>
2020-08-29 20:08:44 +0300

    netlify

---
+++ svelte-typescript-app/src/Filters.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import type { FiltersT } from "./types/types";
+  export let value: FiltersT;
+  export let branches: string[];
+  export let oems: string[];
+</script>
+
+<thead>
+  <tr>
+    <th scope="col">Code</th>
+    <th scope="col">
+      Build
+      <input type="checkbox" bind:checked={value.build} />
+    </th>
+    <th scope="col">
+      Branch
+      <select bind:value={value.branch}>
+        {#each branches as b}
+          <option value={b}>{b}</option>
+        {/each}
+      </select>
+    </th>
+    <th scope="col">
+      OEM
+      <select bind:value={value.oem}>
+        {#each oems as o}
+          <option value={o}>{o}</option>
+        {/each}
+      </select>
+    </th>
+    <th scope="col">Model</th>
+    <th style="width: 200px;" scope="col">Repos</th>
+  </tr>
+</thead>
